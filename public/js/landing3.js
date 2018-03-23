7c3eeea84dc6d2b345d3bc73e9df771e8be1d848
Daniel Sousa <mail@danielsousa.io>
2018-03-23 21:39:11 +0000

    Add third landing sections

--- public/js/landing3.js
+++ public/js/landing3.js
@@ -1,2 +1,10 @@
 $(function() {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<img class="slick-left" src="images/icons/left.png">',
+    nextArrow: '<img class="slick-right" src="images/icons/right.png">'
+  });
 });
